e2b44db074abaa71fe1662b02e88eeb448165019
David Ta <david.ta555@gmail.com>
2022-06-30 14:19:15 -0400

    Create Musician Class

--- Musician.js
+++ Musician.js
@@ -1,14 +1,19 @@
-const {Sequelize, sequelize} = require('./db');
+const { Sequelize, sequelize } = require("./db");
 
 // TODO - define the Musician model
 
 /* 
 
-This will involve calling the sequelize.define() method, and passing in two arguments to the method - the first one is a string 'musician', the second is an object with two kep/values pairs.  They are pair1: name, with the value1: Sequelize.STRING, and key2: intrument and value2: Sequelize.STRING
+This will involve calling the sequelize.define() method, and passing in two arguments to the method - 
+    the first one is a string 'musician', the second is an object with two kep/values pairs.  
+They are pair1: name, with the value1: Sequelize.STRING, and key2: intrument and value2: Sequelize.STRING
 
 */
-let Musician;
+let Musician = sequelize.define("musician", {
+  name: Sequelize.STRING,
+  instrument: Sequelize.STRING,
+});
 
 module.exports = {
-    Musician
+  Musician,
 };
