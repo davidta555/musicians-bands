514f5b3c4ec18bdb73ba0496b382954188b4cf9e
David Ta <david.ta555@gmail.com>
2022-06-30 14:18:57 -0400

    Create Band Class

--- Band.js
+++ Band.js
@@ -1,14 +1,19 @@
-const {Sequelize, sequelize} = require('./db');
+const { Sequelize, sequelize } = require("./db");
 
 // TODO - define the Band model
 
 /* 
 
-This will involve calling the sequelize.define() method, and passing in two arguments to the method - the first one is a string 'band', the second is an object with two kep/values pairs.  They are pair1: name, with the value1: Sequelize.STRING, and key2: genre and value2: Sequelize.STRING
+This will involve calling the sequelize.define() method, and passing in two arguments to the method - 
+    the first one is a string 'band', the second is an object with two kep/values pairs.  
+They are pair1: name, with the value1: Sequelize.STRING, and key2: genre and value2: Sequelize.STRING
 
 */
-let Band;
+let Band = sequelize.define("band", {
+  name: Sequelize.STRING,
+  genre: Sequelize.STRING,
+});
 
 module.exports = {
-    Band
+  Band,
 };
